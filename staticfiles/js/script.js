8410f7c3c14f79d0f5dc2197af8db72847adfd55
Abhijeet <abhigupta7b@gmail.com>
2022-08-16 18:51:42 +0530

    Theme Fix

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -9,9 +9,8 @@ if(theme == null){
 }
 
 let themeDots = document.getElementsByClassName('theme-dot')
-
-
 for (var i=0; themeDots.length > i; i++){
+
 	themeDots[i].addEventListener('click', function(){
 		let mode = this.dataset.mode
 		console.log('Option clicked:', mode)
@@ -21,19 +20,19 @@ for (var i=0; themeDots.length > i; i++){
 
 function setTheme(mode){
 	if(mode == 'light'){
-		document.getElementById('theme-style').href = 'default.css'
+		document.getElementById('theme-style').href = static + '/default.css'
 	}
 
 	if(mode == 'blue'){
-		document.getElementById('theme-style').href = 'blue.css'
+		document.getElementById('theme-style').href = static + '/blue.css'
 	}
 
 	if(mode == 'green'){
-		document.getElementById('theme-style').href = 'green.css'
+		document.getElementById('theme-style').href = static + '/green.css'
 	}
 
 	if(mode == 'purple'){
-		document.getElementById('theme-style').href = 'purple.css'
+		document.getElementById('theme-style').href = static + '/purple.css'
 	}
 
 	localStorage.setItem('theme', mode)
